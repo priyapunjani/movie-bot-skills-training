656c9f005440c80f86ba640943e9f782b585b98c
priyapunjani <50814954+priyapunjani@users.noreply.github.com>
2019-06-20 16:29:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   // You can add your own open weather map token here (https://home.openweathermap.org/api_keys)
   OPENWEATHER_TOKEN: process.env.OPENWEATHER_TOKEN || '',
   // You can add your own tmdb token here (https://www.themoviedb.org/)
-  MOVIEDB_TOKEN: process.env.MOVIEDB_TOKEN || '',
+  MOVIEDB_TOKEN: process.env.MOVIEDB_TOKEN || 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZGM1ODA0NjkyNjMwNDUxYWExOWExMGEwNTVmODNkNCIsInN1YiI6IjVkMDc3MDc3YzNhMzY4NzdkYjIzMzY1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r82m6l1iwSQ_A3do0tm3KZ6OFSzk1iitQn1w_cAl23Y',
   PORT: process.env.PORT || 5000,
 };
